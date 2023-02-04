d7bf7be54098513e2bc852c58a799b062a0fb6bc
samuel-ma <mabilmakur@gmail.com>
2023-02-04 20:20:20 +0200

    More components added to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Email from './components/Email'
+import Faqs from './components/Faqs'
 import Hero from './components/Hero'
 import Hero2 from './components/Hero2'
 import Navbar from './components/Navbar'
@@ -16,6 +18,8 @@ function App() {
       {/* <Hero2/> */}
       <Stats/>
       <Services/>
+      <Faqs/>
+      <Email/>
     </div>
   )
 }
